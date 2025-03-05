4f0461796ed58628c461118513f6d7ff7a8d835f
LeonelMoscoso <127239807+LeonelMoscoso@users.noreply.github.com>
2025-03-05 21:36:16 +0000

    se carga setup_actividad2

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="actividad2",
+    version="0.0.1",
+    author="leonel Moscoso Yovany Patino",
+    author_email="",
+    description="",
+    py_modules=["actividad2"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests"
+    ]
+) 
