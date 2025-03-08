12601997d7f4cad200f52052c2af6325d6e34f08
LeonelMoscoso <127239807+LeonelMoscoso@users.noreply.github.com>
2025-03-08 00:09:14 +0000

    otro ajuste

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="actividad_2",
+    name="actividad2",
     version="0.0.1",
     author="leonel Moscoso Yovany Patino",
     author_email="",
     description="",
-    py_modules=["actividad_2"],
+    py_modules=["actividad2"],
     install_requires=[
         "pandas",
         "openpyxl",
